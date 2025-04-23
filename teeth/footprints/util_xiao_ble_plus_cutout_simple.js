763449bef891671a1e2a5ab49dfcb2259478eab5
jusdisgi <hunter.l.cook@gmail.com>
2025-04-22 23:49:03 -0500

    Add through-hole array for xiao breakouts, clean up simple cutout, fix nets.

--- teeth/footprints/util_xiao_ble_plus_cutout_simple.js
+++ teeth/footprints/util_xiao_ble_plus_cutout_simple.js
@@ -48,28 +48,76 @@ fp.push(`(property "Reference" "${p.ref}" ${p.ref_hide} (at 0 0 ${p.r}) (layer "
 //
 //fp.push(`(pad "1" smd roundrect (at ${(flip ? 6.223 : -10.287)} -7.6165 ${p.r + 180}) (size 2.032 0.95) (layers "${(flip ? "B" : "F")}.Cu" "${(flip ? "B" : "F")}.Mask" "${(flip ? "B" : "F")}.Paste") (roundrect_rratio 0.1) (thermal_bridge_angle 45) ${p.P1})`);
 fp.push(`(pad "1" smd roundrect (at ${(flip ? 9.8 : -9.8)} -7.6165 ${p.r + 180}) (size 2.032 0.75) (layers "${(flip ? "B" : "F")}.Cu" "${(flip ? "B" : "F")}.Mask" "${(flip ? "B" : "F")}.Paste") (roundrect_rratio 0.1) (chamfer_ratio 0.5) (chamfer top_left bottom_left) (thermal_bridge_angle 45) ${p.P1})`);
+fp.push(`(pad "101" smd roundrect (at ${(flip ? 8.8 : -8.8)} -7.6165 ${p.r + 180}) (size 1 0.5) (layers "F.Paste") (roundrect_rratio 0.5))`);
+
 fp.push(`(pad "2" smd roundrect (at ${(flip ? 9.8 : -9.8)} -5.0765 ${p.r + 180}) (size 2.032 0.75) (layers "${(flip ? "B" : "F")}.Cu" "${(flip ? "B" : "F")}.Mask" "${(flip ? "B" : "F")}.Paste") (roundrect_rratio 0.1) (chamfer_ratio 0.5) (chamfer top_left bottom_left) (thermal_bridge_angle 45) ${p.P2})`);
+fp.push(`(pad "102" smd roundrect (at ${(flip ? 8.8 : -8.8)} -5.0765 ${p.r + 180}) (size 1 0.5) (layers "F.Paste") (roundrect_rratio 0.5))`);
+
 fp.push(`(pad "3" smd roundrect (at ${(flip ? 9.8 : -9.8)} -2.5365 ${p.r + 180}) (size 2.032 0.75) (layers "${(flip ? "B" : "F")}.Cu" "${(flip ? "B" : "F")}.Mask" "${(flip ? "B" : "F")}.Paste") (roundrect_rratio 0.1) (chamfer_ratio 0.5) (chamfer top_left bottom_left) (thermal_bridge_angle 45) ${p.P3})`);
+fp.push(`(pad "103" smd roundrect (at ${(flip ? 8.8 : -8.8)} -2.5365 ${p.r + 180}) (size 1 0.5) (layers "F.Paste") (roundrect_rratio 0.5))`);
+
 fp.push(`(pad "4" smd roundrect (at ${(flip ? 9.8 : -9.8)} 0.0035 ${p.r + 180}) (size 2.032 0.75) (layers "${(flip ? "B" : "F")}.Cu" "${(flip ? "B" : "F")}.Mask" "${(flip ? "B" : "F")}.Paste") (roundrect_rratio 0.1) (chamfer_ratio 0.5) (chamfer top_left bottom_left) (thermal_bridge_angle 45) ${p.P4})`);
+fp.push(`(pad "104" smd roundrect (at ${(flip ? 8.8 : -8.8)} 0.0035 ${p.r + 180}) (size 1 0.5) (layers "F.Paste") (roundrect_rratio 0.5))`);
+
 fp.push(`(pad "5" smd roundrect (at ${(flip ? 9.8 : -9.8)} 2.5435 ${p.r + 180}) (size 2.032 0.75) (layers "${(flip ? "B" : "F")}.Cu" "${(flip ? "B" : "F")}.Mask" "${(flip ? "B" : "F")}.Paste") (roundrect_rratio 0.1) (chamfer_ratio 0.5) (chamfer top_left bottom_left) (thermal_bridge_angle 45) ${p.P5})`);
+fp.push(`(pad "105" smd roundrect (at ${(flip ? 8.8 : -8.8)} 2.5435 ${p.r + 180}) (size 1 0.5) (layers "F.Paste") (roundrect_rratio 0.5))`);
+
 fp.push(`(pad "6" smd roundrect (at ${(flip ? 9.8 : -9.8)} 5.0835 ${p.r + 180}) (size 2.032 0.75) (layers "${(flip ? "B" : "F")}.Cu" "${(flip ? "B" : "F")}.Mask" "${(flip ? "B" : "F")}.Paste") (roundrect_rratio 0.1) (chamfer_ratio 0.5) (chamfer top_left bottom_left) (thermal_bridge_angle 45) ${p.P6})`);
+fp.push(`(pad "106" smd roundrect (at ${(flip ? 8.8 : -8.8)} 5.0835 ${p.r + 180}) (size 1 0.5) (layers "F.Paste") (roundrect_rratio 0.5))`);
+
 fp.push(`(pad "7" smd roundrect (at ${(flip ? 9.8 : -9.8)} 7.6235 ${p.r + 180}) (size 2.032 0.75) (layers "${(flip ? "B" : "F")}.Cu" "${(flip ? "B" : "F")}.Mask" "${(flip ? "B" : "F")}.Paste") (roundrect_rratio 0.1) (chamfer_ratio 0.5) (chamfer top_left bottom_left) (thermal_bridge_angle 45) ${p.P7})`);
+fp.push(`(pad "107" smd roundrect (at ${(flip ? 8.8 : -8.8)} 7.6235 ${p.r + 180}) (size 1 0.5) (layers "F.Paste") (roundrect_rratio 0.5))`);
+
 fp.push(`(pad "8" smd roundrect (at ${(flip ? -9.8 : 9.8)} 7.6235 ${p.r}) (size 2.032 0.75) (layers "${(flip ? "B" : "F")}.Cu" "${(flip ? "B" : "F")}.Mask" "${(flip ? "B" : "F")}.Paste") (roundrect_rratio 0.1) (chamfer_ratio 0.5) (chamfer top_left bottom_left) (thermal_bridge_angle 45) ${p.P8})`);
+fp.push(`(pad "108" smd roundrect (at ${(flip ? -8.8 : 8.8)} 7.6235 ${p.r + 180}) (size 1 0.5) (layers "F.Paste") (roundrect_rratio 0.5))`);
+
 fp.push(`(pad "9" smd roundrect (at ${(flip ? -9.8 : 9.8)} 5.0835 ${p.r}) (size 2.032 0.75) (layers "${(flip ? "B" : "F")}.Cu" "${(flip ? "B" : "F")}.Mask" "${(flip ? "B" : "F")}.Paste") (roundrect_rratio 0.1) (chamfer_ratio 0.5) (chamfer top_left bottom_left) (thermal_bridge_angle 45) ${p.P9})`);
+fp.push(`(pad "109" smd roundrect (at ${(flip ? -8.8 : 8.8)} 5.0835 ${p.r + 180}) (size 1 0.5) (layers "F.Paste") (roundrect_rratio 0.5))`);
+
 fp.push(`(pad "10" smd roundrect (at ${(flip ? -9.8 : 9.8)} 2.5435 ${p.r}) (size 2.032 0.75) (layers "${(flip ? "B" : "F")}.Cu" "${(flip ? "B" : "F")}.Mask" "${(flip ? "B" : "F")}.Paste") (roundrect_rratio 0.1) (chamfer_ratio 0.5) (chamfer top_left bottom_left) (thermal_bridge_angle 45) ${p.P10})`);
+fp.push(`(pad "110" smd roundrect (at ${(flip ? -8.8 : 8.8)} 2.5435 ${p.r + 180}) (size 1 0.5) (layers "F.Paste") (roundrect_rratio 0.5))`);
+
 fp.push(`(pad "11" smd roundrect (at ${(flip ? -9.8 : 9.8)} 0.0035 ${p.r}) (size 2.032 0.75) (layers "${(flip ? "B" : "F")}.Cu" "${(flip ? "B" : "F")}.Mask" "${(flip ? "B" : "F")}.Paste") (roundrect_rratio 0.1) (chamfer_ratio 0.5) (chamfer top_left bottom_left) (thermal_bridge_angle 45) ${p.P11})`);
+fp.push(`(pad "111" smd roundrect (at ${(flip ? -8.8 : 8.8)} 0.0035 ${p.r + 180}) (size 1 0.5) (layers "F.Paste") (roundrect_rratio 0.5))`);
+
 fp.push(`(pad "12" smd roundrect (at ${(flip ? -9.8 : 9.8)} -2.5365 ${p.r}) (size 2.032 0.75) (layers "${(flip ? "B" : "F")}.Cu" "${(flip ? "B" : "F")}.Mask" "${(flip ? "B" : "F")}.Paste") (roundrect_rratio 0.1) (chamfer_ratio 0.5) (chamfer top_left bottom_left) (thermal_bridge_angle 45) ${p.P12})`);
+fp.push(`(pad "112" smd roundrect (at ${(flip ? -8.8 : 8.8)} -2.5365 ${p.r + 180}) (size 1 0.5) (layers "F.Paste") (roundrect_rratio 0.5))`);
+
 fp.push(`(pad "13" smd roundrect (at ${(flip ? -9.8 : 9.8)} -5.0765 ${p.r}) (size 2.032 0.75) (layers "${(flip ? "B" : "F")}.Cu" "${(flip ? "B" : "F")}.Mask" "${(flip ? "B" : "F")}.Paste") (roundrect_rratio 0.1) (chamfer_ratio 0.5) (chamfer top_left bottom_left) (thermal_bridge_angle 45) ${p.P13})`);
+fp.push(`(pad "113" smd roundrect (at ${(flip ? -8.8 : 8.8)} -5.0765 ${p.r + 180}) (size 1 0.5) (layers "F.Paste") (roundrect_rratio 0.5))`);
+
 fp.push(`(pad "14" smd roundrect (at ${(flip ? -9.8 : 9.8)} -7.6165 ${p.r}) (size 2.032 0.75) (layers "${(flip ? "B" : "F")}.Cu" "${(flip ? "B" : "F")}.Mask" "${(flip ? "B" : "F")}.Paste") (roundrect_rratio 0.1) (chamfer_ratio 0.5) (chamfer top_left bottom_left) (thermal_bridge_angle 45) ${p.P14})`);
+fp.push(`(pad "114" smd roundrect (at ${(flip ? -8.8 : 8.8)} -7.6165 ${p.r + 180}) (size 1 0.5) (layers "F.Paste") (roundrect_rratio 0.5))`);
+
 fp.push(`(pad "15" smd roundrect (at ${(flip ? 9.4 : -9.4)} -6.3465 ${p.r + 180}) (size 1.232 0.75) (layers "${(flip ? "B" : "F")}.Cu" "${(flip ? "B" : "F")}.Mask" "${(flip ? "B" : "F")}.Paste") (roundrect_rratio 0.1) (chamfer_ratio 0.5) (chamfer top_left bottom_left) (thermal_bridge_angle 45) ${p.P15})`);
+fp.push(`(pad "115" smd roundrect (at ${(flip ? 8.8 : -8.8)} -6.3565 ${p.r + 180}) (size 1 0.5) (layers "F.Paste") (roundrect_rratio 0.5))`);
+
 fp.push(`(pad "16" smd roundrect (at ${(flip ? 9.4 : -9.4)} -3.8065 ${p.r + 180}) (size 1.232 0.75) (layers "${(flip ? "B" : "F")}.Cu" "${(flip ? "B" : "F")}.Mask" "${(flip ? "B" : "F")}.Paste") (roundrect_rratio 0.1) (chamfer_ratio 0.5) (chamfer top_left bottom_left) (thermal_bridge_angle 45) ${p.P16})`);
+fp.push(`(pad "116" smd roundrect (at ${(flip ? 8.8 : -8.8)} -3.8065 ${p.r + 180}) (size 1 0.5) (layers "F.Paste") (roundrect_rratio 0.5))`);
+
 fp.push(`(pad "17" smd roundrect (at ${(flip ? 9.4 : -9.4)} -1.2665 ${p.r + 180}) (size 1.232 0.75) (layers "${(flip ? "B" : "F")}.Cu" "${(flip ? "B" : "F")}.Mask" "${(flip ? "B" : "F")}.Paste") (roundrect_rratio 0.1) (chamfer_ratio 0.5) (chamfer top_left bottom_left) (thermal_bridge_angle 45) ${p.P17})`);
+fp.push(`(pad "117" smd roundrect (at ${(flip ? 8.8 : -8.8)} -1.2665 ${p.r + 180}) (size 1 0.5) (layers "F.Paste") (roundrect_rratio 0.5))`);
+
 fp.push(`(pad "18" smd roundrect (at ${(flip ? 9.4 : -9.4)} 1.2735 ${p.r + 180}) (size 1.232 0.75) (layers "${(flip ? "B" : "F")}.Cu" "${(flip ? "B" : "F")}.Mask" "${(flip ? "B" : "F")}.Paste") (roundrect_rratio 0.1) (chamfer_ratio 0.5) (chamfer top_left bottom_left) (thermal_bridge_angle 45) ${p.P18})`);
+fp.push(`(pad "118" smd roundrect (at ${(flip ? 8.8 : -8.8)} 1.2735 ${p.r + 180}) (size 1 0.5) (layers "F.Paste") (roundrect_rratio 0.5))`);
+
 fp.push(`(pad "19" smd roundrect (at ${(flip ? 9.4 : -9.4)} 3.8135 ${p.r + 180}) (size 1.232 0.75) (layers "${(flip ? "B" : "F")}.Cu" "${(flip ? "B" : "F")}.Mask" "${(flip ? "B" : "F")}.Paste") (roundrect_rratio 0.1) (chamfer_ratio 0.5) (chamfer top_left bottom_left) (thermal_bridge_angle 45) ${p.P19})`);
+fp.push(`(pad "119" smd roundrect (at ${(flip ? 8.8 : -8.8)} 3.8135 ${p.r + 180}) (size 1 0.5) (layers "F.Paste") (roundrect_rratio 0.5))`);
+
 fp.push(`(pad "20" smd roundrect (at ${(flip ? 9.4 : -9.4)} 6.3535 ${p.r + 180}) (size 1.232 0.75) (layers "${(flip ? "B" : "F")}.Cu" "${(flip ? "B" : "F")}.Mask" "${(flip ? "B" : "F")}.Paste") (roundrect_rratio 0.1) (chamfer_ratio 0.5) (chamfer top_left bottom_left) (thermal_bridge_angle 45) ${p.P20})`);
+fp.push(`(pad "120" smd roundrect (at ${(flip ? 8.8 : -8.8)} 6.3535 ${p.r + 180}) (size 1 0.5) (layers "F.Paste") (roundrect_rratio 0.5))`);
+
+fp.push(`(pad "121" smd roundrect (at ${(flip ? -8.8 : 8.8)} 6.3535 ${p.r + 180}) (size 1 0.5) (layers "F.Paste") (roundrect_rratio 0.5))`);
 fp.push(`(pad "21" smd roundrect (at ${(flip ? -9.4 : 9.4)} 6.3535 ${p.r}) (size 1.232 0.75) (layers "${(flip ? "B" : "F")}.Cu" "${(flip ? "B" : "F")}.Mask" "${(flip ? "B" : "F")}.Paste") (roundrect_rratio 0.1) (chamfer_ratio 0.5) (chamfer top_left bottom_left) (thermal_bridge_angle 45) ${p.P21})`);
+
+fp.push(`(pad "122" smd roundrect (at ${(flip ? -8.8 : 8.8)} 3.8135 ${p.r + 180}) (size 1 0.5) (layers "F.Paste") (roundrect_rratio 0.5))`);
 fp.push(`(pad "22" smd roundrect (at ${(flip ? -9.4 : 9.4)} 3.8135 ${p.r}) (size 1.232 0.75) (layers "${(flip ? "B" : "F")}.Cu" "${(flip ? "B" : "F")}.Mask" "${(flip ? "B" : "F")}.Paste") (roundrect_rratio 0.1) (chamfer_ratio 0.5) (chamfer top_left bottom_left) (thermal_bridge_angle 45) ${p.P22})`);
+
 fp.push(`(pad "23" smd roundrect (at ${(flip ? -9.4 : 9.4)} 1.2735 ${p.r}) (size 1.232 0.75) (layers "${(flip ? "B" : "F")}.Cu" "${(flip ? "B" : "F")}.Mask" "${(flip ? "B" : "F")}.Paste") (roundrect_rratio 0.1) (chamfer_ratio 0.5) (chamfer top_left bottom_left) (thermal_bridge_angle 45) ${p.P23})`);
+fp.push(`(pad "123" smd roundrect (at ${(flip ? -8.8 : 8.8)} 1.2735 ${p.r + 180}) (size 1 0.5) (layers "F.Paste") (roundrect_rratio 0.5))`);
+
+
+
 
 //left side
 //fp.push(`(fp_line (start -8.89 8.6625) (end -8.89 -8.6625) (stroke (width 0.05) (type solid)) (layer "F.SilkS"))`);
---
+++ teeth/footprints/xiao_ble_plus_breakout_holes.js
@@ -0,0 +1,143 @@
+// Arduino ProMicro atmega32u4au
+// Params
+//  orientation: default is down
+//    if down, power led will face the pcb
+//    if up, power led will face away from pcb
+
+module.exports = {
+  params: {
+    designator: 'MCU',
+    orientation: 'down',
+    P1: { type: 'net', value: 'P0.02' },
+    P2: { type: 'net', value: 'P0.03' },
+    P3: { type: 'net', value: 'P0.28' },
+    P4: { type: 'net', value: 'P0.29' },
+    P5: { type: 'net', value: 'P0.04' },
+    P6: { type: 'net', value: 'P0.05' },
+    P7: { type: 'net', value: 'P1.11' },
+    P8: { type: 'net', value: 'P1.12' },
+    P9: { type: 'net', value: 'P1.13' },
+    P10: { type: 'net', value: 'P1.14' },
+    P11: { type: 'net', value: 'P1.15' },
+    P12: { type: 'net', value: '3V3' },
+    P13: { type: 'net', value: 'GND' },
+    P14: { type: 'net', value: '5V' },
+    P15: { type: 'net', value: 'P0.15' },
+    P16: { type: 'net', value: 'P0.19' },
+    P17: { type: 'net', value: 'P1.01' },
+    P18: { type: 'net', value: 'P0.09' },
+    P19: { type: 'net', value: 'P0.10' },
+    P20: { type: 'net', value: 'P0.31' },
+    P21: { type: 'net', value: 'P1.03' },
+    P22: { type: 'net', value: 'P1.05' },
+    P23: { type: 'net', value: 'P1.07' },
+    P24: { type: 'net', value: 'DIO' },
+    P25: { type: 'net', value: 'CLK' },
+    P26: { type: 'net', value: 'RST' },
+    P27: { type: 'net', value: 'GND' },
+    P28: { type: 'net', value: 'BAT+' }
+    // RAW: {type: 'net', value: 'RAW'},
+    // GND: {type: 'net', value: 'GND'},
+    // RST: {type: 'net', value: 'RST'},
+    // VCC: {type: 'net', value: 'VCC'},
+    // P21: {type: 'net', value: 'P21'},
+    // P20: {type: 'net', value: 'P20'},
+    // P19: {type: 'net', value: 'P19'},
+    // P18: {type: 'net', value: 'P18'},
+    // P15: {type: 'net', value: 'P15'},
+    // P14: {type: 'net', value: 'P14'},
+    // P16: {type: 'net', value: 'P16'},
+    // P10: {type: 'net', value: 'P10'},
+    // P1: {type: 'net', value: 'P1'},
+    // P0: {type: 'net', value: 'P0'},
+    // P2: {type: 'net', value: 'P2'},
+    // P3: {type: 'net', value: 'P3'},
+    // P4: {type: 'net', value: 'P4'},
+    // P5: {type: 'net', value: 'P5'},
+    // P6: {type: 'net', value: 'P6'},
+    // P7: {type: 'net', value: 'P7'},
+    // P8: {type: 'net', value: 'P8'},
+    // P9: {type: 'net', value: 'P9'}
+  },
+  body: p => {
+    const standard = `
+      (module XIAO_Breakout_Holes (layer F.Cu) (tedit 5B307E4C)
+      ${p.at /* parametric position */}
+
+      ${'' /* footprint reference */}
+      (fp_text reference "${p.ref}" (at 0 0) (layer F.SilkS) ${p.ref_hide} (effects (font (size 1.27 1.27) (thickness 0.15))))
+      (fp_text value "" (at 0 0) (layer F.SilkS) hide (effects (font (size 1.27 1.27) (thickness 0.15))))
+      `
+    function pins(def_neg, def_pos) {
+      return `
+        
+        ${''/* pin names */}
+        (fp_text user 5V (at -8.97 ${def_pos}12.8 ${p.r + 90}) (layer F.SilkS) (effects (font (size 0.8 0.8) (thickness 0.15))))
+        (fp_text user GND (at -6.43 ${def_pos}12.8 ${p.r + 90}) (layer F.SilkS) (effects (font (size 0.8 0.8) (thickness 0.15))))
+        (fp_text user 3V3 (at -3.89 ${def_pos}12.8 ${p.r + 90}) (layer F.SilkS) (effects (font (size 0.8 0.8) (thickness 0.15))))
+        (fp_text user D10 (at -1.35 ${def_pos}12.8 ${p.r + 90}) (layer F.SilkS) (effects (font (size 0.8 0.8) (thickness 0.15))))
+        (fp_text user D20 (at 1.19 ${def_pos}12.8 ${p.r + 90}) (layer F.SilkS) (effects (font (size 0.8 0.8) (thickness 0.15))))
+        (fp_text user D9 (at 3.83 ${def_pos}12.8 ${p.r + 90}) (layer F.SilkS) (effects (font (size 0.8 0.8) (thickness 0.15))))
+        (fp_text user D19 (at 6.27 ${def_pos}12.8 ${p.r + 90}) (layer F.SilkS) (effects (font (size 0.8 0.8) (thickness 0.15))))
+        (fp_text user D8 (at 8.81 ${def_pos}12.8 ${p.r + 90}) (layer F.SilkS) (effects (font (size 0.8 0.8) (thickness 0.15))))
+        (fp_text user D18 (at 11.35 ${def_pos}12.8 ${p.r + 90}) (layer F.SilkS) (effects (font (size 0.8 0.8) (thickness 0.15))))
+        (fp_text user D7 (at 13.89 ${def_pos}12.8 ${p.r + 90}) (layer F.SilkS) (effects (font (size 0.8 0.8) (thickness 0.15))))
+        
+        (fp_text user BAT (at 18.97 ${def_neg}5.0 ${p.r + 90}) (layer F.SilkS) (effects (font (size 0.8 0.8) (thickness 0.15))))
+      
+        (fp_text user D0 (at -8.97 ${def_neg}12.8 ${p.r + 90}) (layer F.SilkS) (effects (font (size 0.8 0.8) (thickness 0.15))))
+        (fp_text user D11 (at -6.43 ${def_neg}12.8 ${p.r + 90}) (layer F.SilkS) (effects (font (size 0.8 0.8) (thickness 0.15))))
+        (fp_text user D1 (at -3.89 ${def_neg}12.8 ${p.r + 90}) (layer F.SilkS) (effects (font (size 0.8 0.8) (thickness 0.15))))
+        (fp_text user D12 (at -1.35 ${def_neg}12.8 ${p.r + 90}) (layer F.SilkS) (effects (font (size 0.8 0.8) (thickness 0.15))))
+        (fp_text user D2 (at 1.19 ${def_neg}12.8 ${p.r + 90}) (layer F.SilkS) (effects (font (size 0.8 0.8) (thickness 0.15))))
+        (fp_text user D13 (at 3.83 ${def_neg}12.8 ${p.r + 90}) (layer F.SilkS) (effects (font (size 0.8 0.8) (thickness 0.15))))
+        (fp_text user D3 (at 6.27 ${def_neg}12.8 ${p.r + 90}) (layer F.SilkS) (effects (font (size 0.8 0.8) (thickness 0.15))))
+        (fp_text user D14 (at 8.81 ${def_neg}12.8 ${p.r + 90}) (layer F.SilkS) (effects (font (size 0.8 0.8) (thickness 0.15))))
+        (fp_text user D4 (at 11.35 ${def_neg}12.8 ${p.r + 90}) (layer F.SilkS) (effects (font (size 0.8 0.8) (thickness 0.15))))
+        (fp_text user D15 (at 13.89 ${def_neg}12.8 ${p.r + 90}) (layer F.SilkS) (effects (font (size 0.8 0.8) (thickness 0.15))))
+        (fp_text user D5 (at 16.43 ${def_neg}12.8 ${p.r + 90}) (layer F.SilkS) (effects (font (size 0.8 0.8) (thickness 0.15))))
+        (fp_text user D16 (at 18.97 ${def_neg}12.8 ${p.r + 90}) (layer F.SilkS) (effects (font (size 0.8 0.8) (thickness 0.15))))
+        (fp_text user D6 (at 21.43 ${def_neg}12.8 ${p.r + 90}) (layer F.SilkS) (effects (font (size 0.8 0.8) (thickness 0.15))))
+      
+        ${''/* and now the actual pins */}
+        (pad 14 thru_hole rect (at -8.97 ${def_pos}14.62 ${p.r}) (size 1.7526 1.7526) (drill 1.0922) (layers *.Cu *.SilkS *.Mask) ${p.P14})
+        (pad 13 thru_hole circle (at -6.43 ${def_pos}14.62 0) (size 1.7526 1.7526) (drill 1.0922) (layers *.Cu *.SilkS *.Mask) ${p.P13})
+        (pad 12 thru_hole circle (at -3.89 ${def_pos}14.62 0) (size 1.7526 1.7526) (drill 1.0922) (layers *.Cu *.SilkS *.Mask) ${p.P12})
+        (pad 11 thru_hole circle (at -1.35 ${def_pos}14.62 0) (size 1.7526 1.7526) (drill 1.0922) (layers *.Cu *.SilkS *.Mask) ${p.P11})
+        (pad 23 thru_hole circle (at 1.19 ${def_pos}14.62 0) (size 1.7526 1.7526) (drill 1.0922) (layers *.Cu *.SilkS *.Mask) ${p.P23})
+        (pad 10 thru_hole circle (at 3.83 ${def_pos}14.62 0) (size 1.7526 1.7526) (drill 1.0922) (layers *.Cu *.SilkS *.Mask) ${p.P10})
+        (pad 22 thru_hole circle (at 6.27 ${def_pos}14.62 0) (size 1.7526 1.7526) (drill 1.0922) (layers *.Cu *.SilkS *.Mask) ${p.P22})
+        (pad 9 thru_hole circle (at 8.81 ${def_pos}14.62 0) (size 1.7526 1.7526) (drill 1.0922) (layers *.Cu *.SilkS *.Mask) ${p.P9})
+        (pad 21 thru_hole circle (at 11.35 ${def_pos}14.62 0) (size 1.7526 1.7526) (drill 1.0922) (layers *.Cu *.SilkS *.Mask) ${p.P21})
+        (pad 8 thru_hole circle (at 13.89 ${def_pos}14.62 0) (size 1.7526 1.7526) (drill 1.0922) (layers *.Cu *.SilkS *.Mask) ${p.P8})
+
+        (pad 28 thru_hole circle (at 16.62 ${def_neg}5.0 0) (size 1.7526 1.7526) (drill 1.0922) (layers *.Cu *.SilkS *.Mask) ${p.P28})
+        
+        (pad 1 thru_hole circle (at -8.97 ${def_neg}14.62 0) (size 1.7526 1.7526) (drill 1.0922) (layers *.Cu *.SilkS *.Mask) ${p.P1})
+        (pad 15 thru_hole circle (at -6.43 ${def_neg}14.62 0) (size 1.7526 1.7526) (drill 1.0922) (layers *.Cu *.SilkS *.Mask) ${p.P15})
+        (pad 2 thru_hole circle (at -3.89 ${def_neg}14.62 0) (size 1.7526 1.7526) (drill 1.0922) (layers *.Cu *.SilkS *.Mask) ${p.P2})
+        (pad 16 thru_hole circle (at -1.35 ${def_neg}14.62 0) (size 1.7526 1.7526) (drill 1.0922) (layers *.Cu *.SilkS *.Mask) ${p.P16})
+        (pad 3 thru_hole circle (at 1.19 ${def_neg}14.62 0) (size 1.7526 1.7526) (drill 1.0922) (layers *.Cu *.SilkS *.Mask) ${p.P3})
+        (pad 17 thru_hole circle (at 3.83 ${def_neg}14.62 0) (size 1.7526 1.7526) (drill 1.0922) (layers *.Cu *.SilkS *.Mask) ${p.P17})
+        (pad 4 thru_hole circle (at 6.27 ${def_neg}14.62 0) (size 1.7526 1.7526) (drill 1.0922) (layers *.Cu *.SilkS *.Mask) ${p.P4})
+        (pad 18 thru_hole circle (at 8.81 ${def_neg}14.62 0) (size 1.7526 1.7526) (drill 1.0922) (layers *.Cu *.SilkS *.Mask) ${p.P18})
+        (pad 5 thru_hole circle (at 11.35 ${def_neg}14.62 0) (size 1.7526 1.7526) (drill 1.0922) (layers *.Cu *.SilkS *.Mask) ${p.P5})
+        (pad 19 thru_hole circle (at 13.89 ${def_neg}14.62 0) (size 1.7526 1.7526) (drill 1.0922) (layers *.Cu *.SilkS *.Mask) ${p.P19})
+        (pad 6 thru_hole circle (at 16.43 ${def_neg}14.62 0) (size 1.7526 1.7526) (drill 1.0922) (layers *.Cu *.SilkS *.Mask) ${p.P6})
+        (pad 20 thru_hole circle (at 18.97 ${def_neg}14.62 0) (size 1.7526 1.7526) (drill 1.0922) (layers *.Cu *.SilkS *.Mask) ${p.P20})
+        (pad 7 thru_hole circle (at 21.43 ${def_neg}14.62 0) (size 1.7526 1.7526) (drill 1.0922) (layers *.Cu *.SilkS *.Mask) ${p.P7})
+      `
+    }
+    if(p.orientation == 'down') {
+      return `
+        ${standard}
+        ${pins('-', '')})
+        `
+    } else {
+      return `
+        ${standard}
+        ${pins('', '-')})
+        `
+    }
+  }
+}
