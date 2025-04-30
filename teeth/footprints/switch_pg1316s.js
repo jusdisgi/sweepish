2f23afda016fede762bb842020ef8a4ba9058f56
Hunter Cook <hunter.l.cook@gmail.com>
2025-04-30 17:48:30 -0500

    Fix PG1316S footprint (add stabilizer holes)

--- teeth/footprints/switch_pg1316s.js
+++ teeth/footprints/switch_pg1316s.js
@@ -1,63 +1,55 @@
-// Kailh PG1316S footprint for Ergogen
-//
-// Author: @huntercook https://github.com/jusdisgi
-// Based on KiCad module CPG1316S01D02_mikeholscher By Mike Holscher
-//
-// The only parameters are "from" and "to" and there is no further configuration available.
-//
-//
 module.exports = {
   params: {
-      designator: 'S',
-      from: undefined,
-      to: undefined
+    designator: 'S',
+    side: 'F',
+    from: undefined,
+    to: undefined
   },
-  body: p => `
+  body: p => {
+    const fp = [];
+    const flip = p.side === "B";
+if (!flip && p.side !== "F") throw new Error('unsupported side: ' + p.side);
 
-  (module PG1316S (layer F.Cu) (tedit 67FEEE99)
+fp.push(`(footprint "CPG1316S01D02_mikeholscher"`);
+fp.push(p.at);
+fp.push(`(layer "${(flip ? "B.Cu" : "F.Cu")}")`);
+fp.push(`(property "Reference" "${p.ref}" ${p.ref_hide} (at 0 0 ${p.r}) (layer "${p.side}.SilkS") (effects (font (size 1 1) (thickness 0.15))${ p.side === "B" ? " (justify mirror)" : ""}))`);
 
-      ${p.at /* parametric position */}
-      (attr smd)
-      ${'' /* footprint reference */}
-      (fp_text reference "${p.ref}" (at 0 0) (layer F.SilkS) ${p.ref_hide} (effects (font (size 1.27 1.27) (thickness 0.15))))
-      (fp_text value "" (at 0 0) (layer F.SilkS) hide (effects (font (size 1.27 1.27) (thickness 0.15))))
-      
-      ${''/* corner marks */}
-      (fp_line (start -7 -6) (end -7 -7) (layer F.SilkS) (width 0.15))
-      (fp_line (start -7 7) (end -6 7) (layer F.SilkS) (width 0.15))
-      (fp_line (start -6 -7) (end -7 -7) (layer F.SilkS) (width 0.15))
-      (fp_line (start -7 7) (end -7 6) (layer F.SilkS) (width 0.15))
-      (fp_line (start 7 6) (end 7 7) (layer F.SilkS) (width 0.15))
-      (fp_line (start 7 -7) (end 6 -7) (layer F.SilkS) (width 0.15))
-      (fp_line (start 6 7) (end 7 7) (layer F.SilkS) (width 0.15))
-      (fp_line (start 7 -7) (end 7 -6) (layer F.SilkS) (width 0.15))
+fp.push(`(attr smd)`);
 
-      ${''/* Diode Pocket */} 
-      (fp_poly
-        (pts
-          (xy 3.8 -3.5)
-          (xy 3.8 -1.65)
-          (xy 3.3 -1.15)
-          (xy -2.2 -1.15)
-          (xy -2.2 -3.9)
-          (xy 2.2 -3.9)
-          (xy 2.2 -3.5)
-        )
-        (stroke (width 0.1) (type solid)) (fill none) (layer "Dwgs.User")
-      )
+// Unknown to kicad2ergogen
 
-      ${''/* keycap */}
-      (fp_rect (start -8 -8) (end 8 8)
-        (layer "Dwgs.User") (stroke (width 0.15) (type solid)) (fill none)
-      )
+// Pads
+fp.push(`(pad "1" smd rect (at ${(flip ? 2.5 : -2.5)} 2.65 ${p.r}) (size 1.55 2) (layers "${(flip ? "B" : "F")}.Cu" "${(flip ? "B" : "F")}.Paste" "${(flip ? "B" : "F")}.Mask") (thermal_bridge_angle 45)  ${p.to})`);
+fp.push(`(pad "2" smd rect (at ${(flip ? -2.5 : 2.5)} 2.65 ${p.r}) (size 1.55 2) (layers "${(flip ? "B" : "F")}.Cu" "${(flip ? "B" : "F")}.Paste" "${(flip ? "B" : "F")}.Mask") (thermal_bridge_angle 45)  ${p.from})`);
+fp.push(`(pad "3" smd rect (at ${(flip ? 6.35 : -6.35)} -6 ${p.r}) (size 2 2) (layers "${(flip ? "B" : "F")}.Cu" "${(flip ? "B" : "F")}.Paste" "${(flip ? "B" : "F")}.Mask") (thermal_bridge_angle 45))`);
+fp.push(`(pad "3" smd rect (at ${(flip ? 6.35 : -6.35)} 6 ${p.r}) (size 2 2) (layers "${(flip ? "B" : "F")}.Cu" "${(flip ? "B" : "F")}.Paste" "${(flip ? "B" : "F")}.Mask") (thermal_bridge_angle 45))`);
+fp.push(`(pad "3" smd rect (at ${(flip ? -6.35 : 6.35)} -6 ${p.r}) (size 2 2) (layers "${(flip ? "B" : "F")}.Cu" "${(flip ? "B" : "F")}.Paste" "${(flip ? "B" : "F")}.Mask") (thermal_bridge_angle 45))`);
+fp.push(`(pad "3" smd rect (at ${(flip ? -6.35 : 6.35)} 6 ${p.r}) (size 2 2) (layers "${(flip ? "B" : "F")}.Cu" "${(flip ? "B" : "F")}.Paste" "${(flip ? "B" : "F")}.Mask") (thermal_bridge_angle 45))`);
 
-      ${''/* pads */}
-      (pad "1" smd rect (at -2.5 2.65 ${p.rot}) (size 1.55 2) (layers "F.Cu" "F.Paste" "F.Mask") ${p.to})
-      (pad "2" smd rect (at 2.5 2.65 ${p.rot}) (size 1.55 2) (layers "F.Cu" "F.Paste" "F.Mask") ${p.from})
-      (pad "MP" smd rect (at -6.35 -6 ${p.rot}) (size 2 2) (layers "F.Cu" "F.Paste" "F.Mask"))
-      (pad "MP" smd rect (at -6.35 6 ${p.rot}) (size 2 2) (layers "F.Cu" "F.Paste" "F.Mask"))
-      (pad "MP" smd rect (at 6.35 -6 ${p.rot}) (size 2 2) (layers "F.Cu" "F.Paste" "F.Mask"))
-      (pad "MP" smd rect (at 6.35 6 ${p.rot}) (size 2 2) (layers "F.Cu" "F.Paste" "F.Mask"))
-  )
-  `
+// Drawings on Edge.Cuts
+fp.push(`(fp_circle (center ${(flip ? 5.8 : -5.8)} 2.75) (end ${(flip ? 5.3 : -5.3)} 2.75) (stroke (width 0.1) (type default)) (fill none) (layer "Edge.Cuts") )`);
+fp.push(`(fp_circle (center ${(flip ? -5.8 : 5.8)} -2.75) (end ${(flip ? -6.4 : 6.4)} -2.75) (stroke (width 0.1) (type default)) (fill none) (layer "Edge.Cuts") )`);
+
+// Drawings on F.Fab
+fp.push(`(fp_rect (start ${(flip ? 8 : -8)} 8) (end ${(flip ? -8 : 8)} -8) (stroke (width 0.1) (type default)) (fill none) (layer "${(flip ? "B.Fab" : "F.Fab")}") )`);
+fp.push(`(fp_rect (start ${(flip ? 6.75 : -6.75)} -6.5) (end ${(flip ? -6.75 : 6.75)} 6.5) (stroke (width 0.1) (type default)) (fill none) (layer "${(flip ? "B.Fab" : "F.Fab")}") )`);
+
+// Drawings on Dwgs.User
+fp.push(`(fp_poly (pts (xy ${(flip ? -3.8 : 3.8)} -3.5) (xy ${(flip ? -3.8 : 3.8)} -1.65) (xy ${(flip ? -3.3 : 3.3)} -1.15) (xy ${(flip ? 2.2 : -2.2)} -1.15) (xy ${(flip ? 2.2 : -2.2)} -3.9) (xy ${(flip ? -2.2 : 2.2)} -3.9) (xy ${(flip ? -2.2 : 2.2)} -3.5)) (stroke (width 0.1) (type solid)) (fill none) (layer "Dwgs.User") )`);
+
+// 3D Models
+fp.push(`(model "\${KIPRJMOD}/PG1316S--装配体.STEP" (offset (xyz -4.75 -6.25 -10.25)) (scale (xyz 1 1 1)) (rotate (xyz 0 0 0)))`);
+
+// Properties
+// fp.push(`(property "Reference" "CPG1316" (at 0 0 ${(p.r + 0) % 360}) (unlocked yes) (layer "Cmts.User") (hide yes)  (effects (font (size 1 1) (thickness 0.1)) (justify${ flip ? " mirror" : ""})))`);
+// fp.push(`(property "Value" "CPG1316S01D02_mikeholscher" (at 0 9 ${(p.r + 0) % 360}) (unlocked yes) (layer "Cmts.User") (hide yes)  (effects (font (size 1 1) (thickness 0.15)) (justify${ flip ? " mirror" : ""})))`);
+// fp.push(`(property "Footprint" "Custom:CPG1316S01D02_mikeholscher" (at 0 10.85 ${(p.r + 0) % 360}) (unlocked yes) (layer "${(flip ? "B.Fab" : "F.Fab")}") (hide yes)  (effects (font (size 1.27 1.27)) (justify${ flip ? " mirror" : ""})))`);
+// fp.push(`(property "Datasheet" "" (at 0 0 ${(p.r + 0) % 360}) (unlocked yes) (layer "${(flip ? "B.Fab" : "F.Fab")}") (hide yes)  (effects (font (size 1.27 1.27)) (justify${ flip ? " mirror" : ""})))`);
+// fp.push(`(property "Description" "" (at 0 0 ${(p.r + 0) % 360}) (unlocked yes) (layer "${(flip ? "B.Fab" : "F.Fab")}") (hide yes)  (effects (font (size 1.27 1.27)) (justify${ flip ? " mirror" : ""})))`);
+
+    fp.push(')');
+    return fp.join('\n');
+  }
 }
+
